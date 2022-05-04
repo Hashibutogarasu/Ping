81e50eb65cf7dc7a748fd60633a62db57239ca6c
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2022-05-04 18:22:14 +0900

    プロジェクト ファイルを追加します。

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace Ping
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.PingLevel = new System.Windows.Forms.PictureBox();
+            this.NowPing = new System.Windows.Forms.Label();
+            this.NoticeIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.MainContext = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ExitItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ShowItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.PingLevel)).BeginInit();
+            this.MainContext.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Location = new System.Drawing.Point(16, 15);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(43, 15);
+            this.TitleLabel.TabIndex = 0;
+            this.TitleLabel.Text = "Ping：";
+            // 
+            // PingLevel
+            // 
+            this.PingLevel.Location = new System.Drawing.Point(98, 12);
+            this.PingLevel.Name = "PingLevel";
+            this.PingLevel.Size = new System.Drawing.Size(32, 32);
+            this.PingLevel.TabIndex = 4;
+            this.PingLevel.TabStop = false;
+            // 
+            // NowPing
+            // 
+            this.NowPing.AutoSize = true;
+            this.NowPing.Location = new System.Drawing.Point(65, 15);
+            this.NowPing.Name = "NowPing";
+            this.NowPing.Size = new System.Drawing.Size(22, 15);
+            this.NowPing.TabIndex = 5;
+            this.NowPing.Text = "?%";
+            // 
+            // NoticeIcon
+            // 
+            this.NoticeIcon.Text = "notifyIcon1";
+            this.NoticeIcon.Visible = true;
+            this.NoticeIcon.Click += new System.EventHandler(this.NotifyIconClick);
+            // 
+            // MainContext
+            // 
+            this.MainContext.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ExitItem,
+            this.ShowItem});
+            this.MainContext.Name = "MainContext";
+            this.MainContext.Size = new System.Drawing.Size(104, 48);
+            this.MainContext.Text = "設定";
+            // 
+            // ExitItem
+            // 
+            this.ExitItem.Name = "ExitItem";
+            this.ExitItem.Size = new System.Drawing.Size(180, 22);
+            this.ExitItem.Text = "Exit";
+            this.ExitItem.Click += new System.EventHandler(this.ExitItem_Click);
+            // 
+            // ShowItem
+            // 
+            this.ShowItem.Name = "ShowItem";
+            this.ShowItem.Size = new System.Drawing.Size(180, 22);
+            this.ShowItem.Text = "Show";
+            this.ShowItem.Click += new System.EventHandler(this.ShowItemClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(232, 64);
+            this.Controls.Add(this.NowPing);
+            this.Controls.Add(this.PingLevel);
+            this.Controls.Add(this.TitleLabel);
+            this.Name = "Form1";
+            this.Text = "Ping";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainFormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.PingLevel)).EndInit();
+            this.MainContext.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.PictureBox PingLevel;
+        private System.Windows.Forms.Label NowPing;
+        private System.Windows.Forms.NotifyIcon NoticeIcon;
+        private System.Windows.Forms.ContextMenuStrip MainContext;
+        private System.Windows.Forms.ToolStripMenuItem ExitItem;
+        private System.Windows.Forms.ToolStripMenuItem ShowItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,163 @@
+﻿using Ping.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ping
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            GetFiWiSignal();
+
+            //NotifyIconオブジェクトを作成する
+            //this.componentsが存在しないならば、省略する
+            this.NoticeIcon = new NotifyIcon(this.components);
+            //アイコンを設定する
+            this.NoticeIcon.Icon = GetPingIconFromBitmap(Resources.Ping_4);
+            //NotifyIconをタスクトレイに表示する
+            this.NoticeIcon.Visible = true;
+            //アイコンの上にマウスポインタを移動した時に表示される文字列
+            this.NoticeIcon.Text = "Ping";
+            //アイコンを右クリックしたときに表示するコンテキストメニュー
+            //ContextMenuStrip1はすでに用意されているものとする
+            this.NoticeIcon.ContextMenuStrip = this.MainContext;
+        }
+
+        public void GetFiWiSignal()
+        {
+            Task task = Task.Run(async() => {
+
+                while (true)
+                {
+                    string result = await GetCmdResult("netsh wlan show interface | findstr %");
+                    string ping_per = result.Replace(" ", "").Replace("シグナル", "").Replace(":", "");
+
+                    string ping_per_num = result.Replace(" ", "").Replace("シグナル", "").Replace(":", "").Replace("%", "");
+                    int ping = int.Parse(ping_per_num);
+
+                    Bitmap img = GetPingImageFromPer(ping);
+                    Icon icon = GetPingIconFromBitmap(img);
+
+                    if (this.InvokeRequired)
+                    {
+                        this.Invoke((MethodInvoker)(() => {
+                            NowPing.Text = ping_per;
+                            PingLevel.Image = img;
+                            this.Icon = icon;
+                            this.NoticeIcon.Text = ping_per;
+                        }));
+                    }
+                    else
+                    {
+                        NowPing.Text = result;
+                        PingLevel.Image = img;
+                        this.Icon = icon;
+                        this.NoticeIcon.Text = ping_per;
+                    }
+
+                    await Task.Delay(500);
+                }
+            });
+        }
+
+        private Bitmap GetPingImageFromPer(int per)
+        {
+            Bitmap ping_0 = Properties.Resources.Ping_0;
+            Bitmap ping_1 = Properties.Resources.Ping_1;
+            Bitmap ping_2 = Properties.Resources.Ping_2;
+            Bitmap ping_3 = Properties.Resources.Ping_3;
+            Bitmap ping_4 = Properties.Resources.Ping_4;
+
+            if (per >= 1 && per <= 24)
+            {
+                return ping_1;
+            }
+
+            if (per >= 25 && per <= 49)
+            {
+                return ping_2;
+            }
+
+            if (per >= 50 && per <= 74)
+            {
+                return ping_3;
+            }
+
+            if (per >= 75 && per <= 100)
+            {
+                return ping_4;
+            }
+
+            return ping_0;
+        }
+
+        private Icon GetPingIconFromBitmap(Bitmap bitmap)
+        {
+            Icon icon = Icon.FromHandle(bitmap.GetHicon());
+            return icon;
+        }
+
+        public async Task<string> GetCmdResult(string cmdline)
+        {
+            //Processオブジェクトを作成
+            Process p = new Process();
+
+            //ComSpec(cmd.exe)のパスを取得して、FileNameプロパティに指定
+            p.StartInfo.FileName = Environment.GetEnvironmentVariable("ComSpec");
+            //出力を読み取れるようにする
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardInput = false;
+            //ウィンドウを表示しないようにする
+            p.StartInfo.CreateNoWindow = true;
+            //コマンドラインを指定（"/c"は実行後閉じるために必要）
+            p.StartInfo.Arguments = "/c " + cmdline;
+
+            //起動
+            p.Start();
+
+            //出力を読み取る
+            string results = await p.StandardOutput.ReadToEndAsync();
+
+            //プロセス終了まで待機する
+            //WaitForExitはReadToEndの後である必要がある
+            //(親プロセス、子プロセスでブロック防止のため)
+            p.WaitForExit();
+            p.Close();
+
+            //出力された結果を表示
+            return results;
+        }
+
+        private void ExitItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(1);
+        }
+
+        private void ShowItemClick(object sender, EventArgs e)
+        {
+            this.Visible = true;
+        }
+
+        private void MainFormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.Visible = false;
+        }
+
+        private void NotifyIconClick(object sender, EventArgs e)
+        {
+            this.Visible = true;
+        }
+    }
+}
